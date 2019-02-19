3a761c045701023ccb0a23b8b7b95099ac050770
Jonathan Long <long.jonathan9@gmail.com>
2019-02-19 15:40:41 -0600

    Assignment 6.2 Commit

---
+++ GameOnEntertainment(Assignment 6.2)/GameOnEntertainment.Domain/Entities/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameOnEntertainment.Domain.Entities
+{
+    public class Person
+    {
+        public Guid PersonId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ GameOnEntertainment(Assignment 6.2)/GameOnEntertainment.Shared/Orchestrators/PersonOrchestrator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOnEntertainment.Domain;
+using GameOnEntertainment.Shared.ViewModels;
+
+namespace GameOnEntertainment.Shared.Orchestrators
+{
+    public class PersonOrchestrator
+    {
+        private readonly PlayerContext _playerContext;
+       
+        public PersonOrchestrator()
+        {
+            _playerContext = new PlayerContext();   
+        }
+
+        public List<PersonViewModel> GetAllPersons()
+        {
+            var persons = _playerContext.Persons.Select(x => new PersonViewModel
+            {
+                PersonId = x.PersonId,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                DateCreated = x.DateCreated,
+                Email = x.Email
+            }).ToList();
+
+            return persons;
+        }
+        
+    }
+}
